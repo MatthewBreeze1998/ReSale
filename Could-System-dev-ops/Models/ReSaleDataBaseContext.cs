afca0ad33cecab8fcf1e8109e66a8a2bc5c05712
MATTHEW BREEZE <T7077260@live.tees.ac.uk>
2019-12-08 21:06:17 +0000

    db done

---
+++ Could-System-dev-ops/Models/ReSaleDataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Could_System_dev_ops.Models
+{
+    public class ReSaleDataBaseContext : DbContext
+    {
+
+        public DbSet<ReSaleModel> User { get; set; }
+
+
+        public ReSaleDataBaseContext(DbContextOptions<ReSaleDataBaseContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+    }
+}
---
+++ Could-System-dev-ops/Repo/IRepositry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Could_System_dev_ops.Models;
+
+namespace Could_System_dev_ops.Repo
+{
+    public interface IRepository<User>
+    {
+        bool UpdateObject(ReSaleModel Object);
+
+
+        ReSaleModel CreateObject(ReSaleModel Object);
+
+        IEnumerable<Models.ReSaleModel> GetObject();
+    }
+}
